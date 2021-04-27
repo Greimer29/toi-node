7477655e29887b53499041435ea2a70d3fd7a069
Greimer29 <greimerabelguzman29@gmail.com>
2021-04-27 12:55:28 -0400

    Comandos del yargs

---
+++ porhacer/config/yargs.js
@@ -0,0 +1,19 @@
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion: {
+            demand: true,
+            alias: 'd'
+        }
+    })
+    .command('actualizar', 'Actualiza el estado completado de una tarea', {
+        descripcion: {
+            demand: true,
+            alias: 'd'
+        },
+        completado: {
+            demand: true,
+            alias: 'c'
+        }
+    }).argv
+
+module.exports = { argv };
